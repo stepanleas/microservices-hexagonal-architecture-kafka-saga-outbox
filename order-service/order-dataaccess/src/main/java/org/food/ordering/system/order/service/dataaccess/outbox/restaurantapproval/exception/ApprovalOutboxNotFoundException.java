d1f9b57a28afd3355c13e337d8deb3f5a59d4889
stepanleas <leasstepan1999@gmail.com>
2023-01-24 11:37:16 +0200

    implement outbox data access in order dataaccess module

---
+++ order-service/order-dataaccess/src/main/java/org/food/ordering/system/order/service/dataaccess/outbox/restaurantapproval/exception/ApprovalOutboxNotFoundException.java
@@ -0,0 +1,8 @@
+package org.food.ordering.system.order.service.dataaccess.outbox.restaurantapproval.exception;
+
+public class ApprovalOutboxNotFoundException extends RuntimeException {
+
+    public ApprovalOutboxNotFoundException(String message) {
+        super(message);
+    }
+}
